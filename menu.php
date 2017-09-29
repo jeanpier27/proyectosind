3c8550a23bedd89686c4ddccd8f6a7d61860dc3d
DESKTOP-EAUIKUA\USER <jp27ism@gmail.com>
2017-09-28 21:12:08 -0500

    se modifca el acceso a los menus

--- menu.php
+++ menu.php
@@ -1,31 +1,35 @@
     <div class="full-reset nav-lateral-list-menu">
         <ul class="list-unstyled">
-        <?php if($_SESSION['tipo_usuario']=='SECRETARIO_GENERAL' or $_SESSION['tipo_usuario']=='SECRETARIO_FINANCIERO'){  ?>
+        <?php if($_SESSION['acceso']=='ADMINISTRADOR'){  ?>
           <li><a href="sindnaranjal.php" id="inicio"><i class="zmdi zmdi-home zmdi-hc-fw"></i>&nbsp;&nbsp; Inicio</a></li>
           <?php } ?>
-           <?php if($_SESSION['tipo_usuario']=='SECRETARIO_GENERAL' or $_SESSION['tipo_usuario']=='SECRETARIO_FINANCIERO' or $_SESSION['tipo_usuario']=='TESORERO' or $_SESSION['tipo_usuario']=='SECRETARIA' or $_SESSION['tipo_usuario']=='CONTADOR'){  ?>
+           <?php if($_SESSION['acceso']=='ADMINISTRADOR' or $_SESSION['acceso']=='ADMINISTRATIVO'){  ?>
              <li>
                 <div class="dropdown-menu-button" id="contadministrativo"><i class="zmdi zmdi-case zmdi-hc-fw"></i>&nbsp;&nbsp; ADMINISTRATIVO <i class="zmdi zmdi-chevron-down pull-right zmdi-hc-fw"></i></div>
                  <ul class="list-unstyled selectable" id="contsocio">
                 <li><a href="socios.php" id="socio"><i class="zmdi zmdi-balance zmdi-hc-fw"></i>&nbsp;&nbsp; Socios</a></li>
                 <li><a href="vehiculos.php" id="vehiculo"><i class="zmdi zmdi-truck zmdi-hc-fw"></i>&nbsp;&nbsp; Vehículos</a></li> 
                 <li><a href="beneficio.php" id="alquiler"><i class="zmdi zmdi-assignment-account zmdi-hc-fw"></i>&nbsp;&nbsp;  Beneficios</a></li>
-                <li><a href="usuario.php" id="usuarios"><i class="zmdi zmdi-assignment-account zmdi-hc-fw"></i>&nbsp;&nbsp; Usuarios</a></li>
+               
          <li><a href="recursoshumanos.php" id="rh"><i class="zmdi zmdi-assignment-account zmdi-hc-fw"></i>&nbsp;&nbsp; Recursos humanos</a></li>
                 </ul>
             </li>
             <?php } ?>
-            <?php if($_SESSION['tipo_usuario']=='SECRETARIO_GENERAL' or $_SESSION['tipo_usuario']=='SECRETARIO_FINANCIERO' or $_SESSION['tipo_usuario']=='INSPECTOR' or $_SESSION['tipo_usuario']=='PEDAGOGO' or $_SESSION['tipo_usuario']=='DOCENTE'){  ?>
+            <?php if($_SESSION['acceso']=='ADMINISTRADOR' or $_SESSION['acceso']=='EDUCATIVO'){  ?>
         
             <li>
                 <div class="dropdown-menu-button"><i class="zmdi zmdi-account-add zmdi-hc-fw"></i>&nbsp;&nbsp; EDUCATIVO <i class="zmdi zmdi-chevron-down pull-right zmdi-hc-fw"></i></div>
                 <ul class="list-unstyled" id="conteducativo">
-               <?php if($_SESSION['tipo_usuario']=='SECRETARIO_GENERAL' or $_SESSION['tipo_usuario']=='SECRETARIO_FINANCIERO' or $_SESSION['tipo_usuario']=='INSPECTOR' or $_SESSION['tipo_usuario']=='PEDAGOGO'){  ?>
+               <?php
+                // if($_SESSION['tipo_usuario']=='SECRETARIO_GENERAL' or $_SESSION['tipo_usuario']=='SECRETARIO_FINANCIERO' or $_SESSION['tipo_usuario']=='INSPECTOR' or $_SESSION['tipo_usuario']=='PEDAGOGO'){  
+                ?>
                     <li><a href="docentes.php" id="docentes"><i class="zmdi zmdi-face zmdi-hc-fw"></i>&nbsp;&nbsp; Docentes</a></li>
                     <li><a href="estudiantes.php" id="estudiantes"><i class="zmdi zmdi-male-alt zmdi-hc-fw"></i>&nbsp;&nbsp; Estudiantes</a></li>
                     <li><a href="asignaturas.php" id="asignaturas"><i class="zmdi zmdi-accounts zmdi-hc-fw"></i>&nbsp;&nbsp; Asignaturas</a></li>
                     <li><a href="calificaciones.php" id="calificaciones"><i class="zmdi zmdi-book"></i>&nbsp;&nbsp; Calificaciones</a></li> 
-                   <?php } ?>
+                   <?php 
+               // } 
+               ?>
                    <?php if($_SESSION['tipo_usuario']=='DOCENTE'){  ?>
                     <li><a href="asistencia.php" id="asistencia_estudiantes"><i class="zmdi zmdi-male-alt zmdi-hc-fw"></i>&nbsp;&nbsp; Asistencia</a></li>
                     <li><a href="calificaciones_docentes.php" id="calificaciones_docentes"><i class="zmdi zmdi-book"></i>&nbsp;&nbsp; Calificaciones</a></li>
@@ -34,7 +38,7 @@
                 </ul>
             </li>
             <?php } ?>
-            <?php if($_SESSION['tipo_usuario']=='SECRETARIO_GENERAL' or $_SESSION['tipo_usuario']=='SECRETARIO_FINANCIERO' or $_SESSION['tipo_usuario']=='TESORERO' or $_SESSION['tipo_usuario']=='SECRETARIA' or $_SESSION['tipo_usuario']=='CONTADOR'){  ?>
+            <?php if($_SESSION['acceso']=='ADMINISTRADOR' or $_SESSION['acceso']=='ADMINISTRATIVO'){  ?>
             <li>
                 <div class="dropdown-menu-button"><i class="zmdi zmdi-assignment-o zmdi-hc-fw"></i>&nbsp;&nbsp; CONTABLE <i class="zmdi zmdi-chevron-down pull-right zmdi-hc-fw"></i></div>
                 <ul class="list-unstyled" id="contcontable">
@@ -47,31 +51,32 @@
             </li>
              <?php } ?>
 
-             <?php if($_SESSION['tipo_usuario']=='SECRETARIO_GENERAL' or $_SESSION['tipo_usuario']=='SECRETARIO_FINANCIERO' or $_SESSION['tipo_usuario']=='INSPECTOR' or $_SESSION['tipo_usuario']=='PEDAGOGO' or $_SESSION['tipo_usuario']=='TESORERO' or $_SESSION['tipo_usuario']=='SECRETARIA' or $_SESSION['tipo_usuario']=='CONTADOR'){  ?>
+             <?php if($_SESSION['acceso']=='ADMINISTRADOR' or $_SESSION['acceso']=='ADMINISTRATIVO' or $_SESSION['acceso']=='EDUCATIVO'){  ?>
         <li>
           <div class="dropdown-menu-button"><i class="zmdi zmdi-alarm zmdi-hc-fw"></i>&nbsp;&nbsp; REPORTES <i class="zmdi zmdi-chevron-down pull-right zmdi-hc-fw"></i></div>
             <ul class="list-unstyled" id="contreporte">
-             <?php if($_SESSION['tipo_usuario']=='SECRETARIO_GENERAL' or $_SESSION['tipo_usuario']=='SECRETARIO_FINANCIERO' or $_SESSION['tipo_usuario']=='TESORERO' or $_SESSION['tipo_usuario']=='SECRETARIA' or $_SESSION['tipo_usuario']=='CONTADOR'){  ?>
+             <?php if($_SESSION['acceso']=='ADMINISTRADOR' or $_SESSION['acceso']=='ADMINISTRATIVO'){  ?>
                 <li><a href="reporteinventario.php" id="reporteinventario"><i class="zmdi zmdi-calendar zmdi-hc-fw"></i>&nbsp;&nbsp; Inventario</a></li>
                 <?php } ?>
-                <?php if($_SESSION['tipo_usuario']=='SECRETARIO_GENERAL' or $_SESSION['tipo_usuario']=='SECRETARIO_FINANCIERO' or $_SESSION['tipo_usuario']=='INSPECTOR' or $_SESSION['tipo_usuario']=='PEDAGOGO'){  ?>
+                <?php if($_SESSION['acceso']=='ADMINISTRADOR' or $_SESSION['acceso']=='ADMINISTRATIVO' or $_SESSION['acceso']=='EDUCATIVO'){  ?>
                 <li> <a href="reportedocente.php" id="reportedocente"><i class="zmdi zmdi-time-restore zmdi-hc-fw"></i>&nbsp;&nbsp; Docentes </a> </li>
 
                 <li> <a href="reportealumnos.php" id="reportealumnos"><i class="zmdi zmdi-time-restore zmdi-hc-fw"></i>&nbsp;&nbsp; Alumnos </a> </li>
                 <li>  <a href="reportenotas.php" id="reportenotas"><i class="zmdi zmdi-timer zmdi-hc-fw"></i>&nbsp;&nbsp; Notas </a> </li> 
                 <?php } ?>
                 <!-- <li><a href="reportecertificado.php" id="reportecertificado"><i class="zmdi zmdi-timer zmdi-hc-fw"></i>&nbsp;&nbsp; Certificados </a></li> -->
-                <?php if($_SESSION['tipo_usuario']=='SECRETARIO_GENERAL' or $_SESSION['tipo_usuario']=='SECRETARIO_FINANCIERO' or $_SESSION['tipo_usuario']=='TESORERO' or $_SESSION['tipo_usuario']=='SECRETARIA' or $_SESSION['tipo_usuario']=='CONTADOR'){  ?>
+                <?php if($_SESSION['acceso']=='ADMINISTRADOR' or $_SESSION['acceso']=='ADMINISTRATIVO'){  ?>
                 <li><a href="reportecontable.php" id="reportecontable"><i class="zmdi zmdi-timer zmdi-hc-fw"></i>&nbsp;&nbsp; Contable </a></li>
                 <?php } ?>
                 
             </ul>
         </li>
         <?php } ?>
-        <?php if($_SESSION['tipo_usuario']=='SECRETARIO_GENERAL' or $_SESSION['tipo_usuario']=='SECRETARIO_FINANCIERO'){  ?>
+        <?php if($_SESSION['acceso']=='ADMINISTRADOR'){  ?>
         <li>
         <div class="dropdown-menu-button"  ><i class="zmdi zmdi-wrench"></i>&nbsp;&nbsp; Configuracion <i class="zmdi zmdi-chevron-down pull-right zmdi-hc-fw"></i></div>
         <ul class="list-unstyled" id="contconfig">
+             <li><a href="usuario.php" id="usuarios"><i class="zmdi zmdi-wrench"></i>&nbsp;&nbsp; Usuarios</a></li>
                 <li><a href="confsocio.php" id="confsocio"><i class="zmdi zmdi-wrench"></i>&nbsp;&nbsp; Socio</a></li>
                 <li>  <a href="confcuotassocio.php" id="confcuotassocio"><i class="zmdi zmdi-wrench"></i>&nbsp;&nbsp; Socio Cuotas </a> </li> 
                 <li> <a href="confcontable.php" id="confcontable"><i class="zmdi zmdi-wrench"></i>&nbsp;&nbsp; Contable </a> </li>
