1c852c74201e5eee1cbb6f55bde3614c80b2ca46
DESKTOP-EAUIKUA\USER <jp27ism@gmail.com>
2017-09-28 21:08:01 -0500

    se modifica la consulta para mejorar el acceso

--- login/ingresar.php
+++ login/ingresar.php
@@ -4,13 +4,14 @@ $usuario=$_POST['usuario'];
 $clave=$_POST['password'];
 	require_once("conexion.php"); 
 
-	$sql=mysqli_query($conexion,"SELECT tb_usuarios.*,tb_personas.cedula_ruc,tb_personas.nombre, tb_personas.apellido,tb_tipo_usuario.tipo_usuario FROM `tb_usuarios` inner JOIN tb_personas on tb_usuarios.id_persona=tb_personas.id_persona inner join tb_tipo_usuario on tb_tipo_usuario.id_tipo_usuario=tb_usuarios.id_tipo_usuario where tb_personas.cedula_ruc like '%".$usuario."%'");
+	$sql=mysqli_query($conexion,"SELECT tb_usuarios.*,tb_personas.cedula_ruc,tb_personas.nombre, tb_personas.apellido,tb_tipo_usuario.tipo_usuario FROM `tb_usuarios` inner JOIN tb_personas on tb_usuarios.id_persona=tb_personas.id_persona inner join tb_tipo_usuario on tb_tipo_usuario.id_tipo_usuario=tb_usuarios.id_tipo_usuario where tb_personas.cedula_ruc like '%".$usuario."%' and tb_usuarios.estado='ACTIVO'");
 	 $rowcount=mysqli_num_rows($sql);
 	 if($rowcount==1){
 	 	while($consulta=mysqli_fetch_array($sql)){
 	 		if($clave==$consulta['contraseña']){
 	 			session_start();
 	 			$_SESSION['tipo_usuario'] = $consulta['tipo_usuario'];
+	 			$_SESSION['acceso'] = $consulta['acceso'];
 	 			$_SESSION['usuario']=$usuario;
 	 			$_SESSION['nombres']=$consulta['nombre'].' '.$consulta['apellido'];
 	 			$_SESSION['id_usuario']=$consulta['id_usuarios'];
